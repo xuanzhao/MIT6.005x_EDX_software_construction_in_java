07d074246289fd2a2006c1af00e1f3a51be82052
ken91 <xuanzhao89@gmail.com>
2017-10-06 19:58:08 +0800

    add ignore

--- ps2/src/twitter/Extract.java
+++ ps2/src/twitter/Extract.java
@@ -1,5 +1,10 @@
 package twitter;
 
+import org.junit.Test;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -21,7 +26,20 @@ public class Extract {
      *         every tweet in the list.
      */
     public static Timespan getTimespan(List<Tweet> tweets) {
-        throw new RuntimeException("not implemented");
+//        throw new RuntimeException("not implemented");
+
+        Instant minTimestamps = tweets.get(0).getTimestamp();
+        Instant maxTimestamps = tweets.get(0).getTimestamp();
+
+        for (Tweet t : tweets) {
+            Instant curTweetTimestamp = t.getTimestamp();
+            if (curTweetTimestamp.isBefore(minTimestamps)) {
+                minTimestamps = curTweetTimestamp;
+            } else if (curTweetTimestamp.isAfter(maxTimestamps)) {
+                maxTimestamps = curTweetTimestamp;
+            }
+        }
+        return new Timespan(minTimestamps, maxTimestamps);
     }
 
     /**
@@ -40,11 +58,44 @@ public class Extract {
      *         include a username at most once.
      */
     public static Set<String> getMentionedUsers(List<Tweet> tweets) {
-        throw new RuntimeException("not implemented");
+        Set<String> mentionedUsers = new HashSet<>();
+
+        for (Tweet t : tweets) {
+            String test = t.getText();
+//            test.
+        }
+        return  mentionedUsers;
+
+//        throw new RuntimeException("not implemented");
     }
 
     /* Copyright (c) 2007-2016 MIT 6.005 course staff, all rights reserved.
      * Redistribution of original or derived work requires explicit permission.
      * Don't post any of this code on the web or to a public Github repository.
      */
+
+
+    public static void main(String[] args) {
+        Instant now = Instant.now();
+        System.out.println(now);
+        Instant truncated = now.truncatedTo(ChronoUnit.HOURS);
+        System.out.println(truncated);
+        System.out.println(now.getEpochSecond());
+        System.out.println(now.compareTo(truncated));
+        System.out.println(now.compareTo(now));
+        System.out.println(truncated.compareTo(now));
+        System.out.println(now.isAfter(truncated));
+
+        Instant d3 = Instant.parse("2016-02-17T10:30:00Z");
+        Tweet tweet4 = new Tweet(3, "ken", "this is tweet 4 message@ken@91akihiko", d3);
+        Tweet tweet5 = new Tweet(3, "ken", "this is tweet 5 message@ken.waseda.jp", d3);
+        String text4 = tweet4.getText();
+        String text5 = tweet5.getText();
+        System.out.println(text4);
+        System.out.println(text5);
+        System.out.println(text4.indexOf('@'));
+        System.out.println(text5.indexOf('@'));
+        System.out.println(text5.split("@"));
+        System.out.println(text5.matches("@"));
+    }
 }
--- ps2/test/twitter/ExtractTest.java
+++ ps2/test/twitter/ExtractTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.time.Instant;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
 import org.junit.Test;
@@ -13,34 +14,108 @@ public class ExtractTest {
     /*
      * TODO: your testing strategies for these methods should go here.
      * Make sure you have partitions.
+     *
+     * Test GetTimespanTwoTweets()
+     * 1. input two tweets
+     * 2. input three tweets
+     * return {start=end, start < end}
+     *
+     * Test GetMentionedUsers()
+     * 1. input has no mentionedUsers
+     * 2. input has one mentionedUsers
+     * 3. input has multi mentionedUsers
+     * 4. input has include illegal mentionedUsers
+     * return {null is true, Set<String> with one element, Set<String> with multi elements}
+     *
+     *
+     * Each of these parts is covered by at least one test case below.
      */
-    
+
     private static final Instant d1 = Instant.parse("2016-02-17T10:00:00Z");
     private static final Instant d2 = Instant.parse("2016-02-17T11:00:00Z");
-    
+    private static final Instant d3 = Instant.parse("2016-02-17T10:30:00Z");
+
     private static final Tweet tweet1 = new Tweet(1, "alyssa", "is it reasonable to talk about rivest so much?", d1);
     private static final Tweet tweet2 = new Tweet(2, "bbitdiddle", "rivest talk in 30 minutes #hype", d2);
-    
-    @Test(expected=AssertionError.class)
+    private static final Tweet tweet3 = new Tweet(3, "ken", "this is tweet 3 message@ken", d3);
+    private static final Tweet tweet4 = new Tweet(3, "ken", "this is tweet 4 message@ken@91akihiko", d3);
+
+    private static final Tweet tweet5 = new Tweet(3, "ken", "this is tweet 5 message@ken.waseda.jp", d3);
+
+
+    @Test(expected = AssertionError.class)
     public void testAssertionsEnabled() {
         assert false; // make sure assertions are enabled with VM argument: -ea
     }
-    
+
     @Test
     public void testGetTimespanTwoTweets() {
         Timespan timespan = Extract.getTimespan(Arrays.asList(tweet1, tweet2));
-        
+
         assertEquals("expected start", d1, timespan.getStart());
         assertEquals("expected end", d2, timespan.getEnd());
     }
-    
+
+    @Test
+    public void testGetTimespanOneTweets() {
+        Timespan timespan = Extract.getTimespan(Arrays.asList(tweet1));
+
+        assertEquals("expected start", d1, timespan.getStart());
+        assertEquals("expected end", d1, timespan.getEnd());
+    }
+
+    @Test
+    public void testGetTimespanThreeTweets() {
+        Timespan timespan = Extract.getTimespan(Arrays.asList(tweet1, tweet2, tweet3));
+
+        assertEquals("expected start", d1, timespan.getStart());
+        assertEquals("expected end", d2, timespan.getEnd());
+    }
+
+//    @Test
+//    public void testGetTimespanZeroTweets() {
+//        Timespan timespan = Extract.getTimespan(Arrays.asList());
+//
+//        assertEquals("expected start", null, timespan.getStart());
+//        assertEquals("expected end", null, timespan.getEnd());
+//    }
+
     @Test
     public void testGetMentionedUsersNoMention() {
         Set<String> mentionedUsers = Extract.getMentionedUsers(Arrays.asList(tweet1));
-        
+
         assertTrue("expected empty set", mentionedUsers.isEmpty());
     }
 
+    @Test
+    public void testGetMentionedUsersOneLegalMention() {
+        Set<String> mentionedUsers = Extract.getMentionedUsers(Arrays.asList(tweet1, tweet2, tweet3));
+
+        assertEquals("expected set", makeSet("ken"), mentionedUsers);
+    }
+
+    @Test
+    public void testGetMentionedUsersMultiLegalMention() {
+        Set<String> mentionedUsers = Extract.getMentionedUsers(Arrays.asList(tweet1, tweet2, tweet3, tweet4));
+
+        assertEquals("expected set", makeSet("ken", "91akihiko"), mentionedUsers);
+    }
+
+    @Test
+    public void testGetMentionedUsersIncludeIllegalMention() {
+        Set<String> mentionedUsers = Extract.getMentionedUsers(Arrays.asList(tweet1, tweet2, tweet3, tweet4, tweet5));
+
+        assertEquals("expected set", makeSet("ken", "91akihiko"), mentionedUsers);
+    }
+
+
+    private static Set<String> makeSet(String... elements) {
+        Set<String> set = new HashSet<>();
+        for (String x : elements) {
+            set.add(x);
+        }
+        return set;
+    }
     /*
      * Warning: all the tests you write here must be runnable against any
      * Extract class that follows the spec. It will be run against several staff
